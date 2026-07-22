35e805dc01fef7b2c8b50d4ff7f55fb96b5fa90a
BoboiAzumi <untukherokudeploy@gmail.com>
2026-07-22 22:37:19 +0700

    fix: 404 file upload

--- ragdiva-frontend/src/features/api/file-api.ts
+++ ragdiva-frontend/src/features/api/file-api.ts
@@ -3,7 +3,7 @@ import type { FileResponseType } from "@/types/file-types"
 
 export const fileApi = {
     get: (data: { criteriaId: string, q: string }): Promise<FileResponseType> => apiClient.get(`/file/${data.criteriaId}?q=${data.q}`),
-    post: (data: { criteriaId: string, data: FormData }): Promise<{ message: string }> => apiClient.multipart(`/${data.criteriaId}`, data.data),
+    post: (data: { criteriaId: string, data: FormData }): Promise<{ message: string }> => apiClient.multipart(`/file/${data.criteriaId}`, data.data),
     patch: (data: { id: string, criteriaId: string, data: FormData }): Promise<{ message: string }> => apiClient.multipart(`/file/${data.criteriaId}/${data.id}`, data.data),
     delete: (data: { id: string, criteriaId: string }): Promise<{ message: string }> => apiClient.delete(`/file/${data.criteriaId}/${data.id}`),
 }
